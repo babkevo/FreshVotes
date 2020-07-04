04633dff7c1f60aecc6a57760f79efe05254bbfe
babkevo <njugunamwaura@gmail.com>
2020-07-04 21:21:14 +0200

    Added the vote and comment join tables

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,26 @@
+package com.freshvotes.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class Vote {
+	
+	private VoteId pk;
+	private Boolean UpVote;
+	
+	@EmbeddedId
+	public VoteId getPk() {
+		return pk;
+	}
+	public void setPk(VoteId pk) {
+		this.pk = pk;
+	}
+	public Boolean getUpVote() {
+		return UpVote;
+	}
+	public void setUpVote(Boolean upVote) {
+		UpVote = upVote;
+	}
+
+}
