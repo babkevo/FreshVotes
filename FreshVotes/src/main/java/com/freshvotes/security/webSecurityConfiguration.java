791d1dfe95650931ace37d2edc04c3a4b567fe27
babkevo <njugunamwaura@gmail.com>
2020-07-05 23:33:31 +0200

    Enabling CSRF protection

--- FreshVotes/src/main/java/com/freshvotes/security/webSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/webSecurityConfiguration.java
@@ -29,12 +29,13 @@ public class webSecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable()
+       http
        .authorizeRequests()
        .antMatchers("/").permitAll()
        .anyRequest().hasRole("USER").and()
        .formLogin()
        .loginPage("/login")
+       .defaultSuccessUrl("/dashboard")
        .permitAll()
        .and()
        .logout()
